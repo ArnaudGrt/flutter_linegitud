db306aad5e87555152a80fa860a11f07b36fc67b
ArnaudGrt <arnaud.grt@gmail.com>
2024-11-15 17:01:59 +0100

    🌱 (user) upate mock

--- lib/api/mocks/user.dart
+++ lib/api/mocks/user.dart
@@ -1,36 +1,47 @@
-import 'package:linegitud/models/user.dart';
 
-UserList mockUser() {
-  return UserList(userList: [
-    User(
-      id: 1,
-      name: "Arnaud",
-      password: "000000"
-    ),
-    User(
-      id: 2,
-      name: "Samuel",
-      password: "111111"
-    ),
-    User(
-      id: 3,
-      name: "Marion",
-      password: "222222"
-    ),
-    User(
-      id: 4,
-      name: "Nicolas",
-      password: "333333"
-    ),
-    User(
-      id: 5,
-      name: "Frédéric",
-      password: "444444"
-    ),
-    User(
-      id: 6,
-      name: "Claire",
-      password: "555555"
-    ),
-  ]);
+import 'dart:convert';
+
+mockUser() {
+  return jsonEncode(
+    {
+      "list": [
+        {
+          "id": 1,
+          "name": "Arnaud",
+          "password": "000000",
+          "icon": "sports_martial_arts"
+        },
+        {
+          "id": 2,
+          "name": "Samuel",
+          "password": "111111",
+          "icon": "sports_handball"
+        },
+        {
+          "id": 3,
+          "name": "Marion",
+          "password": "222222",
+          "icon": "sports_gymnastics"
+        },
+        {
+          "id": 4,
+          "name": "Claire",
+          "password": "333333",
+          "icon": "sports_bar"
+        },
+        {
+          "id": 5,
+          "name": "Nicolas",
+          "password": "444444",
+          "icon": "sports_rugby"
+        },
+        {
+          "id": 6,
+          "name": "Frédéric",
+          "password": "555555",
+          "icon": "sports_tennis"
+        }
+      ]
+    }
+  );
 }
