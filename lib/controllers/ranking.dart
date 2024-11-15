2bce0163f4426bcf6844a55a72630b75bfd63bb3
ArnaudGrt <arnaud.grt@gmail.com>
2024-11-15 17:03:07 +0100

    🚧 (ranking) screen & controller

--- lib/controllers/ranking.dart
+++ lib/controllers/ranking.dart
@@ -1,9 +1,77 @@
+import 'dart:async';
+
 import 'package:get/get.dart';
 
+import 'package:linegitud/api/mocks/line.dart';
+import 'package:linegitud/api/mocks/user.dart';
+
+import 'package:linegitud/models/line.dart';
+import 'package:linegitud/models/user.dart';
+
 class UserRankingController extends GetxController {
+  Rx<List> ranking = Rx([]);
+  final durationValue = 2;
+  final isLoading = false.obs;
+
   @override
   void onInit(){
+    initRankingList();
 
     super.onInit();
+  }
+
+  List<dynamic> fetchUsers(){
+    final usersData = mockUser();
+    final usersList = UserList.fromJson(usersData);
+
+    return usersList.namesOnly();
+  }
+
+  LineList fetchLines(){
+    final linesData = mockLine();
+
+    return LineList.fromJson(linesData, ['validated']);
+  }
+
+  List computeUsersLines(){
+    final users = fetchUsers();
+    final lines = fetchLines();
+    List res = [];
+
+    for (final userName in users){
+      final linesCount = lines.linesCount(userName);
+
+      res.add({ userName: linesCount });
+    }
+
+    return res;
+  }
+
+  Future<void> getRankingList(bool withLoader){
+    if(withLoader){
+      toggleLoader(true);
+    }
+
+    Timer(Duration(seconds: durationValue), () {
+      computeUsersLines();
+      toggleLoader(false);
+    });
+
+    return Future.delayed(Duration(seconds: durationValue));
+  }
+
+  void initRankingList(){
+    getRankingList(true);
+  }
+
+  void refreshRankingList(){
+    getRankingList(false);
+  }
+
+  void toggleLoader(bool value){
+    if(isLoading.value == value) return;
+
+    isLoading.value = value;
+    isLoading.refresh();
   }
 }
--- lib/screens/ranking.dart
+++ lib/screens/ranking.dart
@@ -1,13 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import 'package:linegitud/controllers/ranking.dart';
+
 class UserRanking extends StatelessWidget {
-  const UserRanking({super.key});
+  UserRanking({super.key});
+
+  final UserRankingController controller = Get.find();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Lines List")
-    );
+    return Obx(() => showContent());
+  }
+
+  Widget showContent() {
+    // var theme = Theme.of(Get.context!);
+
+    if (controller.isLoading.value) {
+      return const Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+                child: Center(
+              child: CircularProgressIndicator(),
+            ))
+          ],
+        ),
+      );
+    }
+
+    return Container();
   }
 }
