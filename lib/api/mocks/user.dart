06d71ec082c60355c518be208151a94696638026
ArnaudGrt <arnaud.grt@gmail.com>
2024-11-22 15:29:31 +0100

    🌱 (user) add users in mock

--- lib/api/mocks/user.dart
+++ lib/api/mocks/user.dart
@@ -9,37 +9,67 @@ mockUser() {
           "id": 1,
           "name": "Arnaud",
           "password": "000000",
-          "icon": "sports_martial_arts"
+          "avatar": "https://picsum.photos/200?random=1"
         },
         {
           "id": 2,
           "name": "Samuel",
           "password": "111111",
-          "icon": "sports_handball"
+          "avatar": "https://picsum.photos/200?random=2"
         },
         {
           "id": 3,
           "name": "Marion",
           "password": "222222",
-          "icon": "sports_gymnastics"
+          "avatar": "https://picsum.photos/200?random=3"
         },
         {
           "id": 4,
           "name": "Claire",
           "password": "333333",
-          "icon": "sports_bar"
+          "avatar": "https://picsum.photos/200?random=4"
         },
         {
           "id": 5,
           "name": "Nicolas",
           "password": "444444",
-          "icon": "sports_rugby"
+          "avatar": "https://picsum.photos/200?random=5"
         },
         {
           "id": 6,
           "name": "Frédéric",
           "password": "555555",
-          "icon": "sports_tennis"
+          "avatar": "https://picsum.photos/200?random=6"
+        },
+        {
+          "id": 7,
+          "name": "Guilhem",
+          "password": "666666",
+          "avatar": "https://picsum.photos/200?random=7"
+        },
+        {
+          "id": 8,
+          "name": "Marylou",
+          "password": "777777",
+          "avatar": "https://picsum.photos/200?random=8"
+        },
+        {
+          "id": 9,
+          "name": "Philippe",
+          "password": "888888",
+          "avatar": "https://picsum.photos/200?random=9"
+        },
+        {
+          "id": 10,
+          "name": "Alicia",
+          "password": "999999",
+          "avatar": "https://picsum.photos/200?random=10"
+        },
+        {
+          "id": 11,
+          "name": "Maxime",
+          "password": "123456",
+          "avatar": "https://picsum.photos/200?random=11"
         }
       ]
     }
