277f5982db7c7983aa2bd4fdd91e88bf9e4d6ed2
ArnaudGrt <arnaud.grt@gmail.com>
2024-11-29 17:10:16 +0100

    🚀 (new line) replace mock by db request

--- lib/controllers/new_line.dart
+++ lib/controllers/new_line.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
 
-import 'package:linegitud/api/mocks/line.dart';
-import 'package:linegitud/api/mocks/user.dart';
+import 'package:linegitud/controllers/db.dart';
 
 import 'package:linegitud/models/line.dart';
 import 'package:linegitud/models/user.dart';
 
 class NewLineController extends GetxController {
+  final DataBaseController dbController = Get.find();
+
   final formKey = GlobalKey<FormState>();
   Rx<List> users = Rx([]);
 
@@ -18,27 +20,59 @@ class NewLineController extends GetxController {
 
   @override
   void onInit(){
-    fetchUsers();
+    initUsers();
 
     super.onInit();
   }
 
-   void fetchUsers(){
-    final usersData = mockUser();
-    final userList = UserList.fromJson(usersData);
-
-    users.value = userList.getCleanUsers();
+  void initUsers() async {
+    users.value = await fetchUsers();
   }
 
-  LineList fetchLines(){
-    final jsonData = mockLine();
-    return LineList.fromJson(jsonData, ['validated']);
+  Future <List<CleanUser>> fetchUsers() async {
+    final usersQuery = await dbController.database.rawQuery("SELECT name, avatar FROM users");
+
+    final usersArray = [
+      for(final {
+        'name': name as String,
+        'avatar': avatar as String
+      } in usersQuery)
+        CleanUser(name: name, avatar: avatar)
+    ];
+
+    return usersArray.toList();
   }
 
-  void createLine(){
-    final lines = fetchLines();
+  Future<LineList> fetchLines() async {
+    final linesQuery = await dbController.database.query("lines");
+
+    final linesArray = [
+      for(final {
+          'id': id as int,
+          'sender': sender as String,
+          'recipient': recipient as String,
+          'reason': reason as String,
+          'state': state as String,
+          'created_at': createdAt as String
+      } in linesQuery)
+        Line(id: id, reason: reason, sender: sender, recipient: recipient, state: state, createdAt: DateTime.parse(createdAt))
+    ];
+
+    return LineList(lineList: linesArray);
+  }
+
+  void createLine() async {
+    final lines = await fetchLines();
     final linesCount = lines.linesLength();
+    final nextId = linesCount + 1;
+    final createdAt = DateFormat("y/M/d H:m:s").format(DateTime.now());
 
-    Line(id: linesCount + 1, reason: lineReason.value, sender: lineSender.value, recipient: lineRecipient.value, state: "validated", createdAt: DateTime.now());
+    await dbController.database.rawInsert(
+    '''
+      INSERT INTO lines(id, sender, recipient, reason, state, created_at)
+        VALUES
+        (?, ?, ?, ?, ?, ?)
+    ''',
+    [nextId, lineSender.value, lineRecipient.value, lineReason.value, 'validated', createdAt]);
   }
 }
