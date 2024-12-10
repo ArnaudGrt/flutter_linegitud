bcedc075548a393179d6cfe7948e7aa2b92a5c82
ArnaudGrt <arnaud.grt@gmail.com>
2024-12-10 17:06:53 +0100

    🔧 (home) bindings: add users controller

--- lib/bindings/home.dart
+++ lib/bindings/home.dart
@@ -6,11 +6,13 @@ import 'package:linegitud/controllers/home.dart';
 import 'package:linegitud/controllers/new_line.dart';
 import 'package:linegitud/controllers/ranking.dart';
 import 'package:linegitud/controllers/settings.dart';
+import 'package:linegitud/controllers/users.dart';
 
 class HomeBindings implements Bindings {
   @override
   void dependencies() {
     Get.lazyPut(() => SettingsController(), fenix: true);
+    Get.lazyPut(() => UsersController(), fenix: true);
     Get.lazyPut(() => DataBaseController(), fenix: true);
     Get.lazyPut(() => HomeController(), fenix: true);
     Get.lazyPut(() => HistoryController(), fenix: true);
