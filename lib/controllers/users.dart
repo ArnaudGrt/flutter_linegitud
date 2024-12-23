789ee726d30676f27c92c4b87e14cab228c46580
ArnaudGrt <arnaud.grt@gmail.com>
2024-12-23 17:02:09 +0100

    🚀 (users) controller : manage dbresult return class & delete user

--- lib/controllers/users.dart
+++ lib/controllers/users.dart
@@ -15,18 +15,30 @@ class UsersController extends GetxController {
   final userName = "".obs;
   final userAvatar = "".obs;
 
-  Future<DbResult> searchUser() async {
+  Future searchUser() async {
     final formattedUserValue = userSearchValue.value.capitalizeFirst;
 
     final userQuery = await dbController.database.rawQuery(
         "SELECT id, name, avatar FROM users WHERE name = ? LIMIT 1", [formattedUserValue]);
 
     if (userQuery.length == 1) {
-      return DbResult(
+      return DbResultUser(
           success: true,
           user: CleanUser(id: userQuery.first['id'], name: userQuery.first['name'], avatar: userQuery.first['avatar']));
     }
 
     return DbResult(success: false);
+  }
+
+  Future deleteUser(String name) async {
+    final userQuery = await dbController.database.rawDelete(
+      "DELETE FROM users WHERE name = ?", [name]
+    );
+
+    if(userQuery == 1){
+      return DbResult(success: true);
+    }
+
+    return DbResult(success: false, error: "La suppression ne s'est passée comme prévue...");
   }
 }
