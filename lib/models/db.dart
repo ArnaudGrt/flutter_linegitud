75114933b2c88165396e3e19a12ece6156bb40d8
ArnaudGrt <arnaud.grt@gmail.com>
2024-12-23 17:01:18 +0100

    🚀 (dbresult) multiple instances extends generic class

--- lib/models/db.dart
+++ lib/models/db.dart
@@ -3,13 +3,29 @@ import 'package:linegitud/models/user.dart';
 class DbResult{
   bool success;
   String? error;
-  int? id;
-  CleanUser? user;
 
   DbResult({
     required this.success,
     this.error,
-    this.id,
-    this.user
+  });
+}
+
+class DbResultId extends DbResult{
+  int id;
+
+  DbResultId({
+    required super.success,
+    super.error,
+    required this.id
+  });
+}
+
+class DbResultUser extends DbResult{
+  CleanUser user;
+
+  DbResultUser({
+    required super.success,
+    super.error,
+    required this.user
   });
 }
